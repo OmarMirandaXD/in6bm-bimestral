3a4fef584f7ce64c35a54e4f9104dee7e1905555
Informatica <informatica@alumno.local>
2025-03-03 09:31:29 -0600

    usuario finalizado

---
+++ configs/createAdmin.js
@@ -0,0 +1,26 @@
+import { hash } from 'argon2';
+import Usuario from '../src/usuarios/usuario.model.js';
+
+export const createDefaultAdmin = async () => {
+    try {
+        const adminExists = await Usuario.findOne({ role: "ADMIN_ROLE" });
+        if (!adminExists) {
+            const adminData = {
+                name: "Admin",
+                surname: "User",
+                username: "admin",
+                email: "admin@example.com",
+                password: await hash("adminpassword"), 
+                phone: "12345678",
+                role: "ADMIN_ROLE"
+            };
+            await Usuario.create(adminData);
+            console.log("Admin user created successfully");
+        } else {
+            console.log("Admin user already exists");
+        }
+    } catch (err) {
+        console.error("Error creating admin user:", err);
+    }
+};
+
--- configs/server.js
+++ configs/server.js
@@ -7,7 +7,10 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from 'url';
 import { dbConnection } from "./mongo.js";
+import authRoutes from "../src/auth/auth.routes.js";
+import usuarioRoutes from "../src/usuarios/usuario.routes.js";
 import apiLimiter from "../src/middlewares/rate-limit-validator.js";
+import { createDefaultAdmin } from "./createAdmin.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -28,17 +31,24 @@ const middlewares = (app) => {
 const conectarDB = async () => {
     try {
         await dbConnection();
+        await createDefaultAdmin();
     } catch (err) {
         console.log(`Database connection failed: ${err}`);
         process.exit(1);
     }
 };
 
+const routes = (app) => {
+    app.use("/bimestral/v1/auth", authRoutes);
+    app.use("/bimestral/v1/usuario", usuarioRoutes);
+}
+
 export const initServer = async () => {
     const app = express();
     try {
         middlewares(app);
         await conectarDB();
+        routes(app);
         app.listen(process.env.PORT, () => {
             console.log(`Server running on port ${process.env.PORT}`);
         });
